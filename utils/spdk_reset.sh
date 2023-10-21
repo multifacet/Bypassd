3114486d39cacd22ffbcf11a477b497f67421067
SujayYadalam94 <sujay.yadalam@gmail.com>
2023-10-21 14:06:31 -0600

    Utils: SPDK setup only on selected device; SPDK reset doesn't mount

--- utils/spdk_reset.sh
+++ utils/spdk_reset.sh
@@ -2,19 +2,7 @@
 
 SCRIPT_DIR=$(dirname $(realpath $0))
 
-# Ensure device name and mount point are provided
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Please provide device name and mount point."
-    exit 1
-fi
-
-DEV_NAME=$1
-MOUNT_POINT=$2
-
 # Reset spdk config
 sudo bash $SCRIPT_DIR/../spdk/scripts/setup.sh reset
 
 sleep 1
-
-# Mount device back to mount point
-bash $SCRIPT_DIR/mount_dev.sh $DEV_NAME $MOUNT_POINT
--- utils/spdk_setup.sh
+++ utils/spdk_setup.sh
@@ -13,6 +13,9 @@ DEV_NAME=$1
 # Unmount device from mount point
 bash $SCRIPT_DIR/unmount_dev.sh $DEV_NAME
 
+DEV_ID=$(basename $DEV_NAME)
+DEV_PCI_ADDR=$(cat /sys/block/$DEV_ID/device/address)
+
 sleep 1
 # Run spdk setup
-sudo bash $SCRIPT_DIR/../spdk/scripts/setup.sh config
+sudo PCI_ALLOWED=${DEV_PCI_ADDR} bash $SCRIPT_DIR/../spdk/scripts/setup.sh config
